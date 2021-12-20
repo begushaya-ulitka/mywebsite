d2718e8060bdb694700f32fa7caf9d1b225af5eb
ilya <shtanmaerdane@yandex.ru>
2021-12-21 00:16:58 +0400

    fix admin-form

--- resources/views/admin-form.blade.php
+++ resources/views/admin-form.blade.php
@@ -1,35 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Добавить оператора</title>
-	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-	<link rel="stylesheet" href="{{ URL::asset('css/index.css') }}">
-</head>
-<body>  
-	<div class="wrapper">
-		<div class="loginForm__wrapper">
-			<form class="loginForm" method="post" action="{{route('signin')}}">
-				@csrf
-				<div class="loginForm__title">Добавить оператора</div>
-				<div class="loginForm__fields">
-					<div class="loginForm__field">
-						<input class="loginForm__input" name="email" type="email" placeholder="Email">
-					</div>
-					<div class="loginForm__field">
-						<input class="loginForm__input" name="password" type="password" placeholder="Password">
-					</div>
+@extends('main')
+
+@section('content')
+<section class="section">
+	<div class="loginForm__wrapper">
+		<form class="loginForm" method="post" action="">
+			@csrf
+			<div class="loginForm__title">Добавить оператора</div>
+			<div class="loginForm__fields">
+				<div class="loginForm__field">
+					<input class="loginForm__input" name="email" type="email" placeholder="Email">
 				</div>
-				<button class="loginForm__submit" type="submit">
-					<span>Добавить</span>
-				</button>
-			</form>
-			@foreach ($errors->all() as $error)
-			<div class="loginForm__error">{{ $error }}</div>
-			@endforeach
-		</div>
+				<div class="loginForm__field">
+					<input class="loginForm__input" name="password" type="password" placeholder="Password">
+				</div>
+			</div>
+			<button class="loginForm__submit" type="submit">
+				<span>Добавить</span>
+			</button>
+		</form>
+		@foreach ($errors->all() as $error)
+		<div class="loginForm__error">{{ $error }}</div>
+		@endforeach
 	</div>
-</body>
-</html>
+</section>
+@stop
+
+@section('scripts')
+    <script src="{{ URL::asset('js/index.js') }}"></script>
+@stop
+
